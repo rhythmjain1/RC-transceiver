ceffdc6e971957c2ab71267e32c100391b9e3f53
Rhythm Jain <124446653+rhythmjain1@users.noreply.github.com>
2025-08-12 15:23:59 +0530

    Update _6_Channel_Transmitter.ino

--- _6_Channel_Transmitter.ino
+++ _6_Channel_Transmitter.ino
@@ -1,55 +1,68 @@
-/* Code for NODE B */
+// 6 Channel Transmitter
+// Matches the 6-channel receiver structure
 
 #include <SPI.h>
-#include "RF24.h"
+#include <nRF24L01.h>
+#include <RF24.h>
 
-/* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 9 & 10 */
-RF24 radio(9, 10);
-/**********************************************************/
+// Data packet structure (must match receiver)
+struct Signal {
+  byte throttle;
+  byte yaw;
+  byte pitch;
+  byte roll;
+  byte aux1;
+  byte aux2;
+};
 
-byte node_A_address[6] = "NodeA";
-byte node_B_address[6] = "NodeB";
+Signal data;
+
+// Radio: CE pin 7, CSN pin 8
+RF24 radio(7, 8);
+const uint64_t pipeOut = 0xE9E8F0F0E1LL;
+
+// Joystick / potentiometer pins
+#define THROTTLE_PIN A0
+#define YAW_PIN      A1
+#define PITCH_PIN    A2
+#define ROLL_PIN     A3
+#define AUX1_PIN     4   // Button / switch
+#define AUX2_PIN     A4
+
+// Set default control positions
+void ResetData() {
+  data.roll     = 127;
+  data.pitch    = 127;
+  data.throttle = 0;
+  data.yaw      = 127;
+  data.aux1     = 0;
+  data.aux2     = 127;
+}
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println(F("RF24/examples/GettingStarted"));
-
   radio.begin();
-
-  // Set the PA Level low to prevent power supply related issues since this is a
-  // getting_started sketch, and the likelihood of close proximity of the devices. RF24_PA_MAX is default.
-  radio.setPALevel(RF24_PA_LOW);
-
-  // Open a writing and reading pipe on each radio, with opposite addresses
-  radio.openWritingPipe(node_A_address);
-  radio.openReadingPipe(1, node_B_address);
-
-  // Start the radio listening for data
-  radio.startListening();
+  radio.openWritingPipe(pipeOut);
+  radio.setAutoAck(false);
+  radio.setCRCLength(RF24_CRC_8);
+  radio.setDataRate(RF24_250KBPS);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.setChannel(108);
+  ResetData();
 }
 
 void loop() {
-  /****************** Pong Back Role ***************************/
+  // Read and map joystick/potentiometer values
+  data.throttle = map(analogRead(THROTTLE_PIN), 0, 1023, 0, 255);
+  data.yaw      = map(analogRead(YAW_PIN),      0, 1023, 0, 255);
+  data.pitch    = map(analogRead(PITCH_PIN),    0, 1023, 0, 255);
+  data.roll     = map(analogRead(ROLL_PIN),     0, 1023, 0, 255);
 
-  char msg_from_A[20];
+  // Read auxiliary inputs
+  data.aux1     = digitalRead(AUX1_PIN);
+  data.aux2     = map(analogRead(AUX2_PIN),     0, 1023, 0, 255);
 
-  if ( radio.available()) {
-    // Variable for the received timestamp
-    while (radio.available()) {                                   // While there is data ready
-      radio.read( &msg_from_A, sizeof(msg_from_A) );             // Get the payload
-    }
+  // Send packet
+  radio.write(&data, sizeof(Signal));
 
-    radio.stopListening();                                        // First, stop listening so we can talk
-
-    char msg_to_A[20] = "Hello from node_B";
-    radio.write( &msg_to_A, sizeof(msg_to_A) );              // Send the final one back.
-    radio.startListening();                                       // Now, resume listening so we catch the next packets.
-    
-    Serial.print(F("Got message '"));
-    Serial.print(msg_from_A);
-    Serial.print(F("', Sent response '"));
-    Serial.print(msg_to_A);
-    Serial.println(F("'"));
-  }
-
+  delay(20); // 50 Hz update rate
 }
